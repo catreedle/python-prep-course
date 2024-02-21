447c7cfacc0121e0ae20d20af15fb033599e5c38
Purnama S Rahayu <purnamasrahayu@gmail.com>
2024-02-21 20:13:32 +0700

    more stuff

---
+++ more-stuff/square-root.py
@@ -0,0 +1,14 @@
+from math import sqrt, pow
+
+def get_sqrt(x):
+    return sqrt(x)
+
+def square_root(x):
+    return pow(x, 0.5)
+
+def power_to_half(x):
+    return x ** (1/2)
+
+print(get_sqrt(37))
+print(square_root(37))
+print(power_to_half(37))
